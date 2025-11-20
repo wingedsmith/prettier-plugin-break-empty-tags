3438b387c1ea77339c14053a6f9293d91256d24c
Kuznetsov Vitaliy <Vitaly.V.Kuznetsov@kaspersky.com>
2025-11-21 00:46:40 +0300

    fix package, regex,

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
-const { parsers: typescriptParsers } = require('prettier/parser-typescript');
-const { parsers: javascriptParsers } = require('prettier/parser-babel');
 const { parsers: htmlParsers } = require('prettier/parser-html');
 
 function preprocessEmptyTags(text) {
-    const matchPattern = /([ ]*)(<[^>]*[\n]+[^>]*>)(<\/[^>]*>)/gm
+    const matchPattern = /( *)(<[^>]*[\n\r]+[^>]*>) *(<\/[^>]*>)/gm
     const replacePattern = '$1$2\r\n$1$3'
     return text.replace(matchPattern, replacePattern);
 }
 
 module.exports = {
     parsers: {
-        typescript: {
-            ...typescriptParsers.typescript,
-            preprocess: preprocessEmptyTags,
-        },
-        babel: {
-            ...javascriptParsers.babel,
-            preprocess: preprocessEmptyTags,
-        },
         vue: {
             ...htmlParsers.html,
             preprocess: preprocessEmptyTags,
